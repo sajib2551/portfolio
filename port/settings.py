5565246669c1feb7cabe2b3851e4fbab57512d15
sajib2551 <s.m.sajib2551@gmail.com>
2025-11-19 00:33:26 +0600

    host add

--- port/settings.py
+++ port/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h+u!k4shq!1a7&04hng^hqt8&l!23(2tq)@p(=si)nsc7p+jn0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 # Application definition
 
